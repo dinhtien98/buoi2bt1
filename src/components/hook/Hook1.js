0262902fad4e20037a4dbb6b08880ca59cdfb55b
dinhtien98 <ngodinhtien98@gmail.com>
2024-07-22 11:11:31 +0700

    v0.3

--- src/components/hook/Hook1.js
+++ src/components/hook/Hook1.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./hook1.css"
 
 export default function Hook1() {
     const [flag,setFlag]=useState(true)
+    const sidebarRef=useRef()
   return (
-    <div className={flag?"hook1 active":"hook1"}>
-        <h1>Hook 1</h1>
-        <p>Flag: {flag?"true":"false"}</p>
-        <button onClick={()=>setFlag(!flag)}>change flag</button>
-    </div>
+    <>
+        <div className={flag?"hook1 active":"hook1"}>
+            <h1>Hook 1</h1>
+            <p>Flag: {flag?"true":"false"}</p>
+        </div>
+        <div className='meo' ref={sidebarRef}>
+
+        </div>
+        <button onClick={()=>{
+            setFlag(!flag)
+            sidebarRef.current.classList.toggle("active")
+            }}>change flag</button>
+    </>
+    
   )
 }
